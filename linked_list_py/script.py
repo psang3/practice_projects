c43c9604447b1de047acd31158d03fe9f8e938a2
Pathik Sangani <153537533+psang3@users.noreply.github.com>
2025-05-13 23:13:35 -0500

    wrote some linked list code

---
+++ linked_list_py/script.py
@@ -0,0 +1,28 @@
+# Define your Node class below:
+
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+  
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self, next_node):
+    self.next_node =  next_node 
+
+my_node = Node(44)
+
+print(my_node.get_value()) # this will print 44
+
+# Create your LinkedList class below:
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+
+  def get_head_node(self):
+    return self.head_node
+
