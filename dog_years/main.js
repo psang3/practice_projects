2a15945172516a817cde2d8059170ba587515bd1
Pathik Sangani <153537533+psang3@users.noreply.github.com>
2025-05-18 23:58:48 -0500

    basic project code js

---
+++ dog_years/main.js
@@ -0,0 +1,24 @@
+// variable for my age
+var myAge = 19;
+
+// variable for early years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+
+// variable for lateryears
+let laterYears;
+laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(earlyYears)
+console.log(laterYears)
+
+var myAgeinDogYears = earlyYears + laterYears;
+
+var myName = 'Pathik'.toLowerCase();
+console.log(myName)
+
+console.log("My name is " + myName + ". I am " + myAge + " years old in human years which is " + myAgeinDogYears + " years old in dog years.")
