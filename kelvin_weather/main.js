33017a1214080c674d120f8db214ac70ea7f2575
Pathik Sangani <153537533+psang3@users.noreply.github.com>
2025-05-18 23:40:58 -0500

    kelvin basic js code

---
+++ kelvin_weather/main.js
@@ -0,0 +1,16 @@
+// created a variable
+
+var kelvin = 293;
+
+// basically getting celsius
+var celsius;
+celsius = kelvin - 273;
+
+// getting fahrenheit
+let fahrenheit;
+fahrenheit = celsius * (9/5) + 32;
+
+// used floor method to round number
+fahrenheit = Math.floor(fahrenheit)
+
+console.log("The temperature is "+fahrenheit+" degrees Fahrenheit.")
